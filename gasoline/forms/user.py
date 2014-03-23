bd19eb1fdf715e070e0c2e689344bcf4ebdc3cb1
Sylvain Firmery <sylvain@firmery.fr>
2014-03-23 11:18:26 +0100

    rename lazy_gettext to _l

--- gasoline/forms/user.py
+++ gasoline/forms/user.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.babel import lazy_gettext as _
+from flask.ext.babel import lazy_gettext as _l
 from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField, PasswordField
 from wtforms.validators import Required
@@ -9,19 +9,19 @@ from gasoline.models import User
 
 
 class LoginForm(Form):
-    name = TextField(_(u'Username'),
-                     description=_(u'Username'), validators=[Required()])
-    password = PasswordField(_(u'Password'),
-                             description=_(u'Password'),
+    name = TextField(_l(u'Username'),
+                     description=_l(u'Username'), validators=[Required()])
+    password = PasswordField(_l(u'Password'),
+                             description=_l(u'Password'),
                              validators=[Required()])
-    remember_me = BooleanField(_(u'Remember me'),
-                               description=_(u'Remember me'), default=False)
+    remember_me = BooleanField(_l(u'Remember me'),
+                               description=_l(u'Remember me'), default=False)
 
     def validate(self):
         if not Form.validate(self):
             return False
         user = User.objects(name=self.name.data).first()
         if user is None:
-            self.name.errors.append(_('Username or password mismatch.'))
+            self.name.errors.append(_l('Username or password mismatch.'))
             return False
         return True
