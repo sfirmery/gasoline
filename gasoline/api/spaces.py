299c8c4d53f4bae7692ddc3ebaddd5258079910c
Sylvain Firmery <sylvain@firmery.fr>
2014-06-15 22:39:57 +0200

    WIP api for spaces

---
+++ gasoline/api/spaces.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from flask import Blueprint, abort, Response
+from flask.views import MethodView
+# from flask.ext.login import login_required, current_user
+from flask.ext.babel import gettext as _
+
+from gasoline.core.api import (
+    get_json, to_json, from_json, update_from_json)
+# from gasoline.services import acl_service as acl
+from gasoline.models import Space
+from gasoline.models.space import (
+    json_schema_collection, json_schema_resource,
+    rest_uri_collection, rest_uri_resource)
+
+blueprint_api_spaces = Blueprint('api.spaces', __name__)
+route = blueprint_api_spaces.route
+
+logger = logging.getLogger('gasoline')
+
+
+class SpacesAPI(MethodView):
+    # decorators = [login_required]
+
+    # @acl.acl('read')
+    def get(self, name):
+        if name is None:
+            spaces = Space.objects().all()
+            resp = to_json(json_schema_collection, spaces=spaces)
+        else:
+            space = Space.objects(name=name).first()
+            if space is None:
+                abort(404, 'Space not found')
+            resp = to_json(json_schema_collection, spaces=[space])
+        return Response(response=resp, status=200,
+                        mimetype='application/json')
+
+    def post(self):
+        # get json from request
+        json = get_json()
+
+        # reject request for adding more than one space
+        # if len(json['spaces']) > 1:
+        #     logger.debug('space creation failed: too many spaces')
+        #     abort(400, _('Too many space objects.'))
+
+        # create space from json
+        space = from_json(json['spaces'][0], Space,
+                          json_schema_resource)
+
+        # # create space
+        # try:
+        #     space.save()
+        # except:
+        #     logger.exception('')
+        #     logger.debug('space creation failed: database error')
+        #     abort(422, _('Error while creating space.'))
+
+        resp = to_json(json_schema_collection, spaces=[space])
+        return Response(response=resp, status=201,
+                        mimetype='application/json',
+                        headers={'location': space.uri})
+
+    def put(self, name):
+        # get space
+        space = Space.objects(name=name).first()
+
+        # check if space exists
+        if space is None:
+            logger.debug('space update failed: space not found')
+            abort(404, _('Space not found.'))
+
+        # get json from request
+        json = get_json()
+
+        # update space
+        update_from_json(json, space, json_schema_resource)
+
+        resp = to_json(json_schema_collection, spaces=[space])
+        return Response(response=resp, status=200,
+                        mimetype='application/json')
+
+    patch = put
+
+    def delete(self, name):
+        # get space
+        space = Space.objects(name=name).first()
+
+        # check if space exists
+        if space is None:
+            logger.debug('space update failed: space not found')
+            abort(404, _('Space not found.'))
+
+        # delete space
+        try:
+            space.delete()
+        except:
+            logger.debug('space delete failed: database error')
+            abort(422, _('Error while deleting space.'))
+
+        return Response(status=204, mimetype='application/json')
+
+spaces_view = SpacesAPI.as_view('spaces')
+blueprint_api_spaces.\
+    add_url_rule(rest_uri_collection, defaults={'name': None},
+                 view_func=spaces_view,
+                 methods=['GET'])
+blueprint_api_spaces.\
+    add_url_rule(rest_uri_collection,
+                 view_func=spaces_view,
+                 methods=['POST'])
+blueprint_api_spaces.\
+    add_url_rule(rest_uri_resource,
+                 view_func=spaces_view,
+                 methods=['GET', 'PUT', 'PATCH', 'DELETE'])
--- gasoline/models/space.py
+++ gasoline/models/space.py
@@ -3,10 +3,92 @@
 from gasoline.core.extensions import db
 from gasoline.services.acl import ACE
 
+rest_uri_collection = '/api/v1/spaces'
+rest_uri_resource = '{}/<name>'.format(rest_uri_collection)
+
+json_schema_resource = {
+    'title': 'Space resource Schema',
+    'type': 'object',
+    'required': ['name'],
+    'properties': {
+        'name': {'type': 'string'},
+        'description': {'type': 'string'},
+        'uri': {'type': 'string'},
+    },
+}
+
+json_schema_collection = {
+    'title': 'Spaces collection Schema',
+    'type': 'object',
+    'required': ['spaces'],
+    'properties': {
+        'spaces': {
+            'type': 'array',
+            'minItems': 1,
+            'items': json_schema_resource,
+        },
+    },
+}
+
 
 class Space(db.Document):
     name = db.StringField(primary_key=True)
+    description = db.StringField(default='')
     acl = db.ListField(db.EmbeddedDocumentField(ACE))
+
+    @property
+    def uri(self):
+        return rest_uri_resource.\
+            replace("<name>", self.name)
+
+    # def update_from_rest(self, rest):
+    #     if 'name' in rest and rest['name'] != self.name:
+    #         # update id (delete and recreate document)
+    #         self.delete()
+    #         self.name = rest['name']
+    #         self._created = True
+
+    # def to_rest(self):
+    #     """return space in rest friendly format"""
+    #     rest = dict()
+    #     rest['name'] = self.name
+    #     rest['uri'] = self.uri
+    #     return rest
+
+    # @classmethod
+    # def from_rest(cls, rest):
+    #     """create instance from a JSON rest input"""
+    #     data = dict(("%s" % key, value) for key, value in rest.iteritems())
+    #     errors_dict = {}
+
+    #     fields = cls._fields
+    #     for field_name, field in fields.iteritems():
+    #         if field.db_field in data:
+    #             value = data[field.db_field]
+    #             try:
+    #                 data[field_name] = (value if value is None
+    #                                     else field.to_python(value))
+    #                 if field_name != field.db_field:
+    #                     del data[field.db_field]
+    #             except (AttributeError, ValueError), e:
+    #                 errors_dict[field_name] = e
+
+    #     if errors_dict:
+    #         errors = "\n".join(["%s - %s" % (k, v)
+    #                  for k, v in errors_dict.items()])
+    #         msg = ("Invalid data to create a `%s` instance.\n%s"
+    #                % (cls._class_name, errors))
+    #         raise InvalidDocumentError(msg)
+
+    #     obj = cls(**data)
+    #     return obj
+
+    # def update_from_rest(self, rest):
+    #     if 'name' in rest and rest['name'] != self.name:
+    #         # update id (delete and recreate document)
+    #         self.delete()
+    #         self.name = rest['name']
+    #         self._created = True
 
     def __repr__(self):
         return '<Space name=%s>' % (self.name)
