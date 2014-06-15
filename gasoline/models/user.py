637d3309dc3f98e7ec724aa81a449c9524c10e21
Sylvain Firmery <sylvain@firmery.fr>
2014-06-15 22:39:31 +0200

    WIP api for users

--- gasoline/models/user.py
+++ gasoline/models/user.py
@@ -1,16 +1,46 @@
 # -*- coding: utf-8 -*-
 
 from werkzeug import generate_password_hash, check_password_hash
+from flask import url_for
 
 from gasoline.core.extensions import db
+
+rest_uri_collection = '/api/v1/users'
+rest_uri_resource = '{}/<name>'.format(rest_uri_collection)
+
+json_schema_resource = {
+    'title': 'User resource Schema',
+    'anyOf': [
+        {
+            'type': 'object',
+            'required': ['name'],
+            'properties': {
+                'name': {'type': 'string'},
+                'display_name': {'type': 'string'},
+                'uri': {'type': 'string'},
+            },
+        },
+        {'type': 'string'}
+    ]
+}
+
+json_schema_collection = {
+    'title': 'Documents collection Schema',
+    'type': 'object',
+    'required': ['users'],
+    'properties': {
+        'users': {
+            'type': 'array',
+            'minItems': 1,
+            'items': json_schema_resource,
+        },
+    },
+}
 
 
 class User(db.Document):
     name = db.StringField(primary_key=True)
     password = db.StringField()
-
-    def __repr__(self):
-        return '<User %r>' % (self.name)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -31,5 +61,13 @@ class User(db.Document):
         return unicode(self.name)
 
     @property
+    def uri(self):
+        return rest_uri_resource.\
+            replace("<name>", self.name)
+
+    @property
     def display_name(self):
         return self.name
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
