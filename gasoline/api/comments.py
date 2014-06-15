dfbfc9b5b08e0c2a7c18b9d85698b648e9ba8e33
Sylvain Firmery <sylvain@firmery.fr>
2014-06-15 22:43:43 +0200

    WIP api for comments

---
+++ gasoline/api/comments.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from flask import Blueprint, abort, Response
+from flask.views import MethodView
+# from flask.ext.login import login_required, current_user
+from flask.ext.babel import gettext as _
+
+from gasoline.core.api import (
+    get_json, to_json, from_json, update_from_json)
+# from gasoline.services import acl_service as acl
+from gasoline.models import BaseDocument, Comment
+from gasoline.models.comment import (
+    json_schema_collection, json_schema_resource,
+    rest_uri_collection, rest_uri_resource)
+
+blueprint_api_comments = Blueprint('api.comments', __name__)
+route = blueprint_api_comments.route
+
+logger = logging.getLogger('gasoline')
+
+
+class CommentsAPI(MethodView):
+    # decorators = [login_required]
+
+    # @acl.acl('read')
+    def get(self, space, doc_id, comment_id):
+        if comment_id is None:
+            doc = BaseDocument.objects(space=space, id=doc_id).first()
+            comments = doc.comments
+            resp = to_json(json_schema_collection, comments=comments)
+        else:
+            doc = BaseDocument.objects(space=space, id=doc_id).first()
+            try:
+                comment = doc.comments[comment_id]
+            except:
+                abort(404, 'Comment not found')
+            resp = to_json(json_schema_collection,
+                           comments={comment_id: comment})
+        return Response(response=resp, status=200,
+                        mimetype='application/json')
+
+    def post(self, space, doc_id):
+        # get json from request
+        json = get_json()
+
+        # get document
+        try:
+            doc = BaseDocument.objects(space=space, id=doc_id).first()
+            # check if document exists
+            if doc is None:
+                raise
+        except:
+            logger.debug('post comment failed: document not found')
+            abort(404, _('Document not found.'))
+
+        # create comment from json
+        comment = from_json(json['comments'][0], Comment,
+                            json_schema_resource)
+
+        # print 'doc dict before: {}'.format(doc.__dict__)
+        comment_id = doc.add_comment(comment)
+        doc.reload()
+        # print 'doc dict after: {}'.format(doc.__dict__)
+        comment = doc.comments[comment_id]
+
+        resp = to_json(json_schema_collection, comments={comment_id: comment})
+        return Response(response=resp, status=201,
+                        mimetype='application/json',
+                        headers={'location': comment.uri})
+
+    def put(self, space, doc_id, comment_id):
+        # get json from request
+        json = get_json()
+
+        # get document
+        try:
+            doc = BaseDocument.objects(space=space, id=doc_id).first()
+            # check if document exists
+            if doc is None:
+                raise
+        except:
+            logger.debug('post comment failed: document not found')
+            abort(404, _('Document not found.'))
+
+        # check if comment exists
+        try:
+            comment = doc.comments[comment_id]
+        except:
+            logger.debug('comment update failed: comment not found')
+            abort(404, _('Comment not found.'))
+
+        # update comment from json
+        comment = update_from_json(json['comments'][0], comment,
+                                   json_schema_resource)
+
+        # print 'doc dict before: {}'.format(doc.__dict__)
+        doc.update_comment(comment)
+        doc.reload()
+        # print 'doc dict after: {}'.format(doc.__dict__)
+        comment = doc.comments[comment_id]
+
+        resp = to_json(json_schema_collection, comments={comment_id: comment})
+        return Response(response=resp, status=201,
+                        mimetype='application/json',
+                        headers={'location': comment.uri})
+
+    patch = put
+
+    def delete(self, space, doc_id, comment_id):
+        # get document
+        try:
+            doc = BaseDocument.objects(space=space, id=doc_id).first()
+            # check if document exists
+            if doc is None:
+                raise
+        except:
+            logger.debug('post comment failed: document not found')
+            abort(404, _('Document not found.'))
+
+        # # check if comment exists
+        # if comment_id not in doc.comments:
+        #     logger.debug('comment update failed: comment not found')
+        #     abort(404, _('Comment not found.'))
+        try:
+            comment = doc.comments[comment_id]
+        except:
+            logger.debug('comment update failed: comment not found')
+            abort(404, _('Comment not found.'))
+
+        # delete comment
+        try:
+            doc.delete_comment(comment)
+        except:
+            logger.exception('')
+            logger.debug('comment delete failed: database error')
+            abort(422, _('Error while deleting comment.'))
+
+        return Response(status=204, mimetype='application/json')
+
+comments_view = CommentsAPI.as_view('comments')
+blueprint_api_comments.\
+    add_url_rule(rest_uri_collection, defaults={'comment_id': None},
+                 view_func=comments_view,
+                 methods=['GET'])
+blueprint_api_comments.\
+    add_url_rule(rest_uri_collection,
+                 view_func=comments_view,
+                 methods=['POST'])
+blueprint_api_comments.\
+    add_url_rule(rest_uri_resource,
+                 view_func=comments_view,
+                 methods=['GET', 'PUT', 'PATCH', 'DELETE'])
--- gasoline/models/comment.py
+++ gasoline/models/comment.py
@@ -5,15 +5,62 @@ import markdown2
 
 from gasoline.core.extensions import db
 from gasoline.core.signals import event, activity
-from .user import User
+from .user import User, json_schema_resource as json_schema_user
+
+rest_uri_collection = '/api/v1/<space>/documents/<doc_id>/comments'
+rest_uri_resource = '{}/<comment_id>'.format(rest_uri_collection)
+
+json_schema_resource = {
+    'title': 'Comment resource Schema',
+    'type': 'object',
+    'required': ['author', 'content'],
+    'properties': {
+        'id': {'type': 'string'},
+        'author': json_schema_user,
+        'date': {'type': 'string'},
+        'content': {'type': 'string'},
+        'uri': {'type': 'string'},
+    },
+}
+
+json_schema_collection = {
+    'title': 'Comments collection Schema',
+    'type': 'object',
+    'required': ['comments'],
+    'properties': {
+        'comments': {
+            # 'type': 'array',
+            # 'minItems': 1,
+            # 'items': json_schema_resource,
+            'title': 'Comment resource Schema',
+            'type': 'object',
+            'additionalProperties': json_schema_resource,
+        },
+    },
+}
 
 
 class Comment(db.EmbeddedDocument):
+    id = db.StringField()
     author = db.ReferenceField(User)
     date = db.DateTimeField(default=datetime.utcnow)
     content = db.StringField()
 
-    reply = db.ListField(db.EmbeddedDocumentField("Comment"))
+    reply = db.ListField(db.EmbeddedDocumentField('Comment'))
+
+    def __init__(self, **kwargs):
+        # remove date field for new comment
+        if '__auto_convert' not in kwargs:
+            kwargs.pop('date')
+        super(Comment, self).__init__(**kwargs)
+
+    @property
+    def uri(self):
+        print self._instance.__dict__
+        return rest_uri_resource.\
+            replace('<space>', self._instance.space).\
+            replace('<doc_id>', unicode(self._instance.id)).\
+            replace('<comment_id>', self.id)
 
     def __repr__(self):
         return '<Comment author=%s>' % self.author
